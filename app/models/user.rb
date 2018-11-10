2a20c02f569b9c466a9d83637247e6d9854e63c4
Thor77 <thor77@thor77.org>
2018-11-10 15:50:27 +0100

    Apply rubocop formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
-  
+
   include DeviseTokenAuth::Concerns::User
 
   validates :username, presence: true, uniqueness: true
