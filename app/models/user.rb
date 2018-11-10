3f8ca715cc6df6133ad9c95361ea30872102814c
Thor77 <thor77@thor77.org>
2018-11-10 15:46:02 +0100

    Add devise token authentication

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable
+  
+  include DeviseTokenAuth::Concerns::User
+
+  validates :username, presence: true, uniqueness: true
+end
