9964622548732ee71453217664432ee9b168398c
Daniel Schädler <dschaedler@united-internet.de>
2018-11-12 18:26:26 +0100

    Remove redundant user routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'users'
 
-  post '/users', to: 'user#create'
-  post '/users/sign_in', to: 'user#sign_in'
-  get '/users/:username', to: 'user#get'
-  put '/users/:username', to: 'user#put'
-  delete '/users/:username', to: 'user#delete'
-
   post '/tournaments', to: 'tournament#post'
   get '/tournaments/:code', to: 'tournament#get'
   put '/tournaments/:code', to: 'tournament#put'
