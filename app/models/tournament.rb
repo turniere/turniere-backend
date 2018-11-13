20fafab009945e6a230f1306353061ed03acd7d8
Thor77 <thor77@thor77.org>
2018-11-13 16:53:28 +0100

    Generate tournament code if not given

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
+require 'securerandom'
+
 class Tournament < ApplicationRecord
   validates :name, presence: true
   validates :code, presence: true, uniqueness: true
 
+  has_many :teams, dependent: :destroy
   belongs_to :user
+
+  after_initialize do |tournament|
+    tournament.code ||= SecureRandom.hex 3
+  end
 end
