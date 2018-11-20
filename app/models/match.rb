673e12c052fc2b61235b58a4eae7c76b924859eb
Thor77 <thor77@thor77.org>
2018-11-20 16:03:58 +0100

    Validate Stage xor Group present in match

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 class Match < ApplicationRecord
-  belongs_to :stage
-  belongs_to :group
+  belongs_to :stage, optional: true
+  belongs_to :group, optional: true
   has_many :scores, dependent: :destroy
 
   validates :scores, length: { maximum: 2 }
+
+  validate :stage_xor_group
+
+  private
+
+  def stage_xor_group
+    errors.add(:stage_xor_group, 'Stage and Group missing or both present') unless stage.present? ^ group.present?
+  end
 end
