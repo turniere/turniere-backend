29b570bafa3a587163ab63011d888c38b845cdbd
Thor77 <thor77@thor77.org>
2018-11-25 20:08:55 +0100

    Add deserialize_list helper

--- spec/deserialize_helpers.rb
+++ spec/deserialize_helpers.rb
@@ -4,4 +4,10 @@ module DeserializeHelpers
   def deserialize_response(response)
     ActiveModelSerializers::Deserialization.jsonapi_parse(JSON.parse(response.body))
   end
+
+  def deserialize_list(response)
+    JSON.parse(response.body, symbolize_names: true)[:data].map do |raw_obj|
+      raw_obj[:attributes].merge raw_obj.except(:attributes)
+    end
+  end
 end
