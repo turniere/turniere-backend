8e7869b879d7cd9ac43a3b34d1f595bba7f90872
Malaber <32635600+Malaber@users.noreply.github.com>
2018-11-29 11:01:52 +0100

    Improve Factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :user do
-    username { Faker::Internet.username }
-    email { Faker::Internet.email }
+    username { Faker::Internet.unique.username }
+    email { Faker::Internet.unique.email }
   end
 end
