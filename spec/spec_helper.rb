a75087aa6c6ea241cbd0ed43d6bdf78c4ce728e4
Malaber <32635600+Malaber@users.noreply.github.com>
2018-11-29 11:02:25 +0100

    Prepare spec_helper for focused tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,10 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+
+  # only runs tests with " , focus: true   "
+  # config.filter_run focus: true
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
