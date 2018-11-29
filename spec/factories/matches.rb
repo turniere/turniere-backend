34159f7e014fcafd25e5e61004da1ee511c17969
Malaber <32635600+Malaber@users.noreply.github.com>
2018-11-29 11:01:17 +0100

    Add a running playoff Match to Match Factory

--- spec/factories/matches.rb
+++ spec/factories/matches.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :stage_match, aliases: [:match], class: Match do
+  factory :playoff_match, aliases: [:match], class: Match do
     stage
+    factory :running_playoff_match do
+      transient do
+        scores_count { 2 }
+      end
+      after(:create) do |match, evaluator|
+        match.scores = create_list(:score, evaluator.scores_count)
+      end
+      state { 3 }
+    end
   end
 
   factory :group_match, class: Match do
