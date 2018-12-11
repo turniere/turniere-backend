c38066408f4e9440bb2944e34143bbf3b9486f8c
Thor77 <thor77@thor77.org>
2018-12-11 17:06:50 +0100

    Adapt deserialization methods to new schema

--- spec/deserialize_helpers.rb
+++ spec/deserialize_helpers.rb
@@ -2,12 +2,6 @@
 
 module DeserializeHelpers
   def deserialize_response(response)
-    ActiveModelSerializers::Deserialization.jsonapi_parse(JSON.parse(response.body))
-  end
-
-  def deserialize_list(response)
-    JSON.parse(response.body, symbolize_names: true)[:data].map do |raw_obj|
-      raw_obj[:attributes].merge raw_obj.except(:attributes)
-    end
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
