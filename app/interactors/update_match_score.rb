1bdf99ebe9e812d7ae9d7f6445d500daf0978731
Malaber <32635600+Malaber@users.noreply.github.com>
2019-04-16 20:19:50 +0200

    Add UpdateMatchScore Interactor
    This interactor updates the match score and its state accordingly for a running match

---
+++ app/interactors/update_match_score.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class UpdateMatchScore
+  include Interactor
+
+  def call
+    match_score = context.match_score
+    match_score_params = context.match_score_params
+    if match_score.update(match_score_params)
+      match_score.match.state = 'in_progress'
+      context.fail! unless match_score.save
+    else
+      context.fail!
+    end
+  end
+end
