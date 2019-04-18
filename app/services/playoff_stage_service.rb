38425df8237cb5e9e708327ea80af1f38fe51282
Malaber <32635600+Malaber@users.noreply.github.com>
2019-04-18 14:19:19 +0200

    Only support stage count calculation for 1+ teams

--- app/services/playoff_stage_service.rb
+++ app/services/playoff_stage_service.rb
@@ -61,8 +61,8 @@ class PlayoffStageService
   # @param number_of_teams [Integer] the teams number of teams to calculate amount of stages
   # @return [Integer] amount of required stages
   def self.calculate_required_stage_count(number_of_teams)
-    if number_of_teams.zero? || number_of_teams == 1
-      0
+    if number_of_teams == 1
+      1
     else
       # black voodoo magic
       stage_count = Math.log(Utils.next_power_of_two(number_of_teams)) / Math.log(2)
