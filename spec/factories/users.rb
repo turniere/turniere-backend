847f4a1c6978594f74818ad48e47485791aba2c4
Thor77 <thor77@thor77.org>
2019-04-23 10:48:25 +0200

    Add password to User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :user do
     username { Faker::Internet.unique.username }
     email { Faker::Internet.unique.email }
+    password { Faker::Internet.password }
   end
 end
