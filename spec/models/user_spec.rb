3ef3b6985ff6302f8f7df435cb5c76001df94f94
Thor77 <thor77@thor77.org>
2019-04-23 10:46:36 +0200

    Add spec for custom username validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe User, type: :model do
     it { should have_many :tournaments }
   end
 
+  describe 'validation' do
+    subject { create(:user) }
+    it { should validate_presence_of :username }
+    it { should validate_uniqueness_of(:username).case_insensitive }
+  end
+
   it 'has a valid factory' do
     expect(build(:user)).to be_valid
   end
