1fdc292aaa9dce6ed3cef5203bee1ca8d3fbb0d4
Malaber <32635600+Malaber@users.noreply.github.com>
2019-04-24 12:00:17 +0200

    Add method to get all teams within a stage

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -4,4 +4,8 @@ class Stage < ApplicationRecord
   belongs_to :tournament
   has_many :matches, dependent: :destroy
   has_many :groups, dependent: :destroy
+
+  def teams
+    groups.map{|g| g.matches.map{ |m| m.match_scores.map{ |ms| ms.team}}}.flatten.uniq
+  end
 end
