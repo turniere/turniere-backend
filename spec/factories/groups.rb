bbbc198b9c159b0569b089f5fa85cef7fd39e288
Malaber <32635600+Malaber@users.noreply.github.com>
2019-04-27 22:27:38 +0200

    Add multiple factories

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -2,7 +2,15 @@
 
 FactoryBot.define do
   factory :group do
+    transient do
+      match_count { 4 }
+    end
+
     sequence(:number)
     stage
+
+    after(:create) do |group, evaluator|
+      create_list(:group_match, evaluator.match_count, group: group)
+    end
   end
 end
