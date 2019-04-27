3fc344249d2c7fbdb8148ea0c9a44e663fbb04be
Malaber <32635600+Malaber@users.noreply.github.com>
2019-04-27 22:27:38 +0200

    Add multiple factories

--- spec/factories/stages.rb
+++ spec/factories/stages.rb
@@ -3,5 +3,14 @@
 FactoryBot.define do
   factory :stage do
     tournament
+    factory :group_stage do
+      level { -1 }
+      transient do
+        group_count { 4 }
+      end
+      after(:create) do |stage, evaluator|
+        stage.groups = create_list(:group, evaluator.group_count)
+      end
+    end
   end
 end
