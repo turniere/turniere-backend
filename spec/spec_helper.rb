be89152cb191360f9324d875c8ada432676493b2
Thor77 <thor77@thor77.org>
2019-05-02 13:31:04 +0200

    Lint factories before spec suite
    This also removes the rake task and implements Factory linting as
    before(:suite) which is not recommended by the FactoryBot guys, but
    we decided to implement it that way as it does not need to be
    configured within CI explicitly.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,7 @@ RSpec.configure do |config|
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
 
+  config.before(:suite) { FactoryBot.lint }
   # only runs tests with " , focus: true   "
   # config.filter_run focus: true
 
