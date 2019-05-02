babc233412c36c90d50d37104a4a2cd84767873b
Malaber <32635600+Malaber@users.noreply.github.com>
2019-05-02 09:47:26 +0200

    Adds :playoff_stage as factory

--- spec/factories/stages.rb
+++ spec/factories/stages.rb
@@ -12,5 +12,15 @@ FactoryBot.define do
         stage.groups = create_list(:group, evaluator.group_count)
       end
     end
+
+    factory :playoff_stage do
+      level { rand(10) }
+      transient do
+        match_count { 4 }
+      end
+      after(:create) do |stage, evaluator|
+        stage.matches = create_list(:match, evaluator.match_count)
+      end
+    end
   end
 end
