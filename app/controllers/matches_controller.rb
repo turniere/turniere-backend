436adde70613bd19f5c8f1a053ac7d8d4a70630b
Thor77 <thor77@thor77.org>
2019-05-07 15:33:21 +0200

    Set global match var for reuse by other methods

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class MatchesController < ApplicationController
+  before_action :set_match, only: %i[show update]
+
   # GET /matches/1
   def show
-    render json: Match.find(params[:id]), include: ['match_scores.points', 'match_scores.team']
+    render json: @match, include: ['match_scores.points', 'match_scores.team']
+  end
+
+  private
+
+  def set_match
+    @match = Match.find(params[:id])
   end
 end
