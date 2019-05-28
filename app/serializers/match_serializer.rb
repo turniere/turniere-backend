91fe9dba16c64627a173c2115abdb6e1c2d7c00e
Thor77 <thor77@thor77.org>
2019-05-28 23:18:12 +0200

    Add winner attribute to match serializer

--- app/serializers/match_serializer.rb
+++ app/serializers/match_serializer.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class MatchSerializer < ApplicationSerializer
-  attributes :state, :position
+  attributes :state, :position, :winner
+
+  def winner
+    ActiveModelSerializers::SerializableResource.new(object.winner).as_json
+  end
 
   has_many :match_scores
 end
