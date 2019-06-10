e6e07d42073a29e10a03baa5f20924bebbf2e665
Malaber <32635600+Malaber@users.noreply.github.com>
2019-06-10 19:17:40 +0200

    Assign default values on Group Stage creation
    default values are assigned to instant_finalists_amount and
    intermediate_round_participants_amount depending on playoff_teams_amount and group amount

---
+++ app/services/tournament_service.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class TournamentService
+  class << self
+    def calculate_default_amount_of_teams_advancing(playoff_teams_amount, amount_of_groups)
+      # the amount of whole places that advance in a group (e. g. all 1rst places of every group instantly go through)
+      instant_finalists_amount = (playoff_teams_amount.floor / amount_of_groups.floor) * amount_of_groups.floor
+      # the amount of teams that still need to play an intermediate round before advancing to playoffs
+      intermediate_round_participants_amount = (playoff_teams_amount - instant_finalists_amount) * 2
+
+      [instant_finalists_amount, intermediate_round_participants_amount]
+    end
+  end
+end
