8c1acb9cc79f38b0966b80e23373f116ed1d3a5e
Thor77 <thor77@thor77.org>
2019-06-18 12:24:37 +0200

    Add configuration for mail delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,10 +53,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -79,4 +75,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mail
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'api01.turnie.re' }
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: Rails.application.credentials.dig(:mailgun, :api_key),
+    domain: Rails.application.credentials.dig(:mailgun, :domain)
+  }
 end
