0695fba0c4a8425fb81c707d0f8d70c5f57b05ea
Daniel Schädler <dschaedler@united-internet.de>
2024-04-10 21:29:05 +0200

    Configure mailgun for europe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
     api_key: ENV.fetch('MAILGUN_API_KEY'),
-    domain: ENV.fetch('MAILGUN_DOMAIN')
+    domain: ENV.fetch('MAILGUN_DOMAIN'),
+    api_host: 'api.eu.mailgun.net' # Uncomment this line for EU region domains
   }
 
   # Logging to STDOUT (running in docker by default)
