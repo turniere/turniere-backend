fcf276ed3141d1abc4330dfe2d3ea55aeefb9a1a
Malaber <git@schaedler.rocks>
2025-02-12 22:29:11 +0100

    Add test data for beerpong
    (cherry picked from commit c708b86c35114e59f2331ef82d7c6aeca8b6f781)

--- spec/factories/stages.rb
+++ spec/factories/stages.rb
@@ -3,6 +3,7 @@
 FactoryBot.define do
   factory :stage do
     tournament
+
     factory :group_stage do
       level { -1 }
       state { :in_progress }
@@ -22,14 +23,24 @@ FactoryBot.define do
         match_count { 4 }
       end
       after(:create) do |stage, evaluator|
-        # match_count -1 automatically generates 2 ^ stage.level matches
-        # (as this would be the amount of stages present in the real world)
         stage.matches = create_list(evaluator.match_type,
                                     evaluator.match_count == -1 ? 2**stage.level : evaluator.match_count)
         stage.matches.each_with_index do |match, i|
           match.position = i
         end
         stage.save!
+      end
+    end
+
+    factory :decided_group_stage do
+      level { -1 }
+      state { :finished }
+      transient do
+        group_count { 8 }
+        match_factory { :decided_group_match }
+      end
+      after(:create) do |stage, evaluator|
+        stage.groups = create_list(:group, evaluator.group_count, match_factory: evaluator.match_factory)
       end
     end
   end
