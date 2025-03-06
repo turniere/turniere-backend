04143b2d2f116c0e3874b6aa92ffba421f22c886
Malaber <git@schaedler.rocks>
2025-03-06 13:28:50 +0100

    Add tests for group factory

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -23,4 +23,24 @@ RSpec.describe Group, type: :model do
       expect(teams).to match_array(@teams)
     end
   end
+
+  # spec/models/group_spec.rb
+  require 'rails_helper'
+
+  describe 'Factory' do
+    it 'creates a valid group' do
+      group = create(:group)
+      expect(group).to be_valid
+    end
+
+    it 'creates the correct number of matches' do
+      group = create(:group, match_count: 3)
+      expect(group.matches.count).to eq(3)
+    end
+
+    it 'creates group scores for each team' do
+      group = create(:group)
+      expect(group.group_scores.count).to eq(group.teams.count)
+    end
+  end
 end
