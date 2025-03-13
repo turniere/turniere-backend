f7dc8411f4f9489b39d0b3cdbffdff7d841f741d
Malaber <git@schaedler.rocks>
2025-03-13 15:56:01 +0100

    Include timer_end in TournamentSerializer

--- app/serializers/tournament_serializer.rb
+++ app/serializers/tournament_serializer.rb
@@ -2,7 +2,7 @@
 
 class TournamentSerializer < SimpleTournamentSerializer
   attributes :description, :playoff_teams_amount,
-             :instant_finalists_amount, :intermediate_round_participants_amount
+             :instant_finalists_amount, :intermediate_round_participants_amount, :timer_end
   has_many :stages
   has_many :teams
 
