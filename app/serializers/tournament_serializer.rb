963812cb0cd2661ca7a64d060fc77f108bfed6d4
Malaber <git@schaedler.rocks>
2025-03-14 13:23:10 +0100

    Annotate advancing teams that advance from group stage

--- app/serializers/tournament_serializer.rb
+++ app/serializers/tournament_serializer.rb
@@ -4,9 +4,19 @@ class TournamentSerializer < SimpleTournamentSerializer
   attributes :description, :playoff_teams_amount,
              :instant_finalists_amount, :intermediate_round_participants_amount
   has_many :stages
-  has_many :teams
 
   attribute :owner_username do
     object.owner.username
+  end
+
+  attribute :teams do
+    adv_teams = object.group_stage ? GroupStageService.get_advancing_teams(object.group_stage) : []
+    object.teams.map do |team|
+      {
+        id: team.id,
+        name: team.name,
+        advancing_from_group_stage: adv_teams.include?(team)
+      }
+    end
   end
 end
